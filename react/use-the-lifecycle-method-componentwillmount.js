d7b5a2fd88f0414ba5b3ac810f6c667f4522d216
Jayachandran <79501478+JCjaiRN@users.noreply.github.com>
2022-01-22 18:32:34 +0530

    Add files via upload

---
+++ react/use-the-lifecycle-method-componentwillmount.js
@@ -0,0 +1,13 @@
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentWillMount() {
+    // Change code below this line
+  console.log('component will being mounted');
+    // Change code above this line
+  }
+  render() {
+    return <div />
+  }
+};
