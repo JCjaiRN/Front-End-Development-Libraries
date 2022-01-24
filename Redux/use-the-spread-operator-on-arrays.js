00f17f9385b08e94e588f6f51e4533d0fbba91a4
Jayachandran <79501478+JCjaiRN@users.noreply.github.com>
2022-01-24 22:28:36 +0530

    Add files via upload

---
+++ Redux/use-the-spread-operator-on-arrays.js
@@ -0,0 +1,20 @@
+const immutableReducer = (state = ['Do not mutate state!'], action) => {
+  switch(action.type) {
+    case 'ADD_TO_DO':
+      // Don't mutate state here or the tests will fail
+      let arr = [...state,action.todo]
+      return arr;
+
+    default:
+      return state;
+  }
+};
+
+const addToDo = (todo) => {
+  return {
+    type: 'ADD_TO_DO',
+    todo
+  }
+}
+
+const store = Redux.createStore(immutableReducer);
